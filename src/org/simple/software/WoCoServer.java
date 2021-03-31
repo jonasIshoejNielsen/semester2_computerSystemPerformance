9608c4d655da1b53ff55e31a4502101121d35af2
join <join@itu.dk>
2021-03-31 12:25:38 +0200

    refractoring part 1

--- src/org/simple/software/WoCoServer.java
+++ src/org/simple/software/WoCoServer.java
@@ -21,9 +21,6 @@ public class WoCoServer {
 	
 	public static final char SEPARATOR = '$';
 	
-	private HashMap<Integer, StringBuilder> buffer;
-	private HashMap<Integer, HashMap<String, Integer>> results;
-	
 	/**
 	 * Performs the word count on a document. It first converts the document to 
 	 * lower case characters and then extracts words by considering "a-z" english characters
@@ -56,104 +53,8 @@ public class WoCoServer {
 			}
 		}
 	}
-	
-	/**
-	 * Constructor of the server.
-	 */
-	public WoCoServer() {
-		buffer = new HashMap<Integer, StringBuilder>();	
-		results = new HashMap<Integer, HashMap<String, Integer>>();
-	}
-	
-	/**
-	 * This function handles data received from a specific client (TCP connection).
-	 * Internally it will check if the buffer associated with the client has a full
-	 * document in it (based on the SEPARATOR). If yes, it will process the document and
-	 * return true, otherwise it will add the data to the buffer and return false
-	 * @param clientId
-	 * @param dataChunk
-	 * @return A document has been processed or not.
-	 */
-	public boolean receiveData(int clientId, String dataChunk) {
-		
-		StringBuilder sb;
-		
-		if (!results.containsKey(clientId)) {
-			results.put(clientId, new HashMap<String, Integer>());
-		}
-		
-		if (!buffer.containsKey(clientId)) {
-			sb = new StringBuilder();
-			buffer.put(clientId, sb);
-		} else {
-			sb = buffer.get(clientId);
-		}
-		
-		sb.append(dataChunk);
-				
-		if (dataChunk.indexOf(WoCoServer.SEPARATOR)>-1) {
-			//we have at least one line
-			
-			String bufData = sb.toString();
-			
-			int indexNL = bufData.indexOf(WoCoServer.SEPARATOR);
-			
-			String line = bufData.substring(0, indexNL);
-			String rest = (bufData.length()>indexNL+1) ? bufData.substring(indexNL+1) : null;
-			
-			if (indexNL==0) {
-				System.out.println("SEP@"+indexNL+" bufdata:\n"+bufData);
-			}
-			
-			if (rest != null) {
-				System.out.println("more than one line: \n"+rest);
-				try {
-					System.in.read();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				buffer.put(clientId, new StringBuilder(rest));
-			} else {
-				buffer.put(clientId, new StringBuilder());
-			}
-			
-			
-			//word count in line
-			HashMap<String, Integer> wc = results.get(clientId);
-			doWordCount(line, wc);
-			
-			
-			return true;
-			
-		} else {
-			return false;
-		}
-		
-	}
-	
-	/**
-	 * Returns a serialized version of the word count associated with the last
-	 * processed document for a given client. If not called before processing a new
-	 * document, the result is overwritten by the new one.
-	 * @param clientId
-	 * @return
-	 */
-	public String serializeResultForClient(int clientId) {
-		if (results.containsKey(clientId)) {
-			StringBuilder sb = new StringBuilder();
-			HashMap<String, Integer> hm = results.get(clientId);
-			for (String key : hm.keySet()) {
-				sb.append(key+",");
-				sb.append(hm.get(key)+",");
-			}
-			results.remove(clientId);
-			sb.append("\n");
-			return sb.substring(0);
-		} else {
-			return "";
-		}
-	}
-	
+
+
 
 	public static void main(String[] args) throws IOException {
 		
@@ -182,7 +83,7 @@ public class WoCoServer {
 		}
 		
 		
-		WoCoServer server = new WoCoServer();
+		DataHandler dataHandler = new DataHandler();
 		
 		Selector selector = Selector.open(); 
  		
@@ -229,11 +130,11 @@ public class WoCoServer {
 		            if (readCnt>0) {
 		            	String result = new String(bb.array(),0, readCnt);		            
 		            		         						
-						boolean hasResult = server.receiveData(clientId, result);
+						boolean hasResult = dataHandler.receiveData(clientId, result);
 						
 						if (hasResult) {
 							
-							ba = ByteBuffer.wrap(server.serializeResultForClient(clientId).getBytes());
+							ba = ByteBuffer.wrap(dataHandler.serializeResultForClient(clientId).getBytes());
 							client.write(ba);
 						}
 		            } else {
