d5cb75090f7c76e7efbf50c06b20908103758237
join <join@itu.dk>
2021-04-28 08:22:59 +0200

    fixed experiment threads

--- experiment-threads-client.sh
+++ experiment-threads-client.sh
@@ -7,7 +7,11 @@ MAX_CLIENTS=16
 ./run-client.sh $SERVER_HOST "16" "1" true "0" $MIN_CLIENTS $MAX_CLIENTS
 for (( THREADS=1; THREADS<=1; THREADS*=2 ))
 do
-  ./run-client.sh $SERVER_HOST "16" "1" true $THREADS $MIN_CLIENTS $MAX_CLIENTS
+  for (( file=1; file<=2; file++ ))
+  do
+    ./run-client.sh $SERVER_HOST "16" $file true $THREADS $MIN_CLIENTS $MAX_CLIENTS
+    wait
+  done
   wait
 done
 wait
--- experiment-threads-server.sh
+++ experiment-threads-server.sh
@@ -7,7 +7,11 @@ MAX_CLIENTS=16
 ./run-server.sh $SERVER_HOST true "0" "16" "1" $MIN_CLIENTS $MAX_CLIENTS
 for (( THREADS=1; THREADS<=1; THREADS*=2 ))
 do
-  ./run-server.sh $SERVER_HOST true "$THREADS" "16" "1" $MIN_CLIENTS $MAX_CLIENTS
+  for (( file=1; file<=2; file++ ))
+  do
+    ./run-server.sh $SERVER_HOST true "$THREADS" "16" $file $MIN_CLIENTS $MAX_CLIENTS
+    wait
+  done
   wait
 done
 wait
