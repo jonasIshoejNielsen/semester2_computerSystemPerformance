b147daaebebe7b954b3c85f78da19a92309842e3
join <join@itu.dk>
2021-05-09 19:06:30 +0200

    made experiment2 individual

--- experiment-documents-client.sh
+++ experiment-documents-client.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 SERVER_HOST=$1
+index_ops=$2
 THREADS="4"
 NUMBER_OF_CLIENTS=16
 declare -a OPS=(300 90 55 36 25 21 19 16)
-index_ops=0
-for (( dsize=4; dsize<=256; dsize+=36 ))
+dsize=$((4+index_ops*36))
+
+for (( file=1; file<=2; file++ ))
 do
-  echo "dsize=$dsizeStart, index_ops=$index_ops, OPS=${OPS[$index_ops]}"
-  for (( file=1; file<=2; file++ ))
-  do
-    ./run-client.sh $SERVER_HOST $dsize ${OPS[$index_ops]} $file true $THREADS $NUMBER_OF_CLIENTS $NUMBER_OF_CLIENTS
-    wait
-  done
+  echo "dsize=$dsizeStart, index_ops=$index_ops, OPS=${OPS[$index_ops]}, file=$file"
+  ./run-client.sh $SERVER_HOST $dsize ${OPS[$index_ops]} $file true $THREADS $NUMBER_OF_CLIENTS $NUMBER_OF_CLIENTS
+  wait
 done
-wait
-echo "Done"
--- experiment-documents-server.sh
+++ experiment-documents-server.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 SERVER_HOST=$1
+index_ops=$2
 THREADS="4"
 NUMBER_OF_CLIENTS=16
 declare -a OPS=(300 90 55 36 25 21 19 16)
-index_ops=0
-for (( dsize=4; dsize<=256; dsize+=36 ))
+dsize=$((4+index_ops*36))
+
+for (( file=1; file<=2; file++ ))
 do
-  echo "dsize=$dsizeStart, index_ops=$index_ops, OPS=${OPS[$index_ops]}"
-  for (( file=1; file<=2; file++ ))
-  do
-    ./run-server.sh $SERVER_HOST true $THREADS $dsize $file ${OPS[$index_ops]} $NUMBER_OF_CLIENTS $NUMBER_OF_CLIENTS
-    wait
-  done
-  index_ops=$((index_ops+1))
+  echo "dsize=$dsizeStart, index_ops=$index_ops, OPS=${OPS[$index_ops]}, file=$file"
+  ./run-server.sh $SERVER_HOST true $THREADS $dsize $file ${OPS[$index_ops]} $NUMBER_OF_CLIENTS $NUMBER_OF_CLIENTS
+  wait
 done
-wait
 echo "Done"
 
