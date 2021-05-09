b6c778a98e8e4c871b1f728e2064b1c02b58ab3b
join <join@itu.dk>
2021-05-09 14:54:38 +0200

    updated experiment threads

--- experiment-threads-client.sh
+++ experiment-threads-client.sh
@@ -6,11 +6,13 @@ MAX_CLIENTS=32
 ./run-client.sh $SERVER_HOST "16" "125" "1" false "0" $MIN_CLIENTS $MAX_CLIENTS
 ./run-client.sh $SERVER_HOST "16" "125" "1" true "0" $MIN_CLIENTS $MAX_CLIENTS
 ./run-client.sh $SERVER_HOST "16" "125" "1" true "1" $MIN_CLIENTS $MAX_CLIENTS
+./run-client.sh $SERVER_HOST "16" "125" "1" true "4" $MIN_CLIENTS $MAX_CLIENTS
 ./run-client.sh $SERVER_HOST "16" "125" "1" true "8" $MIN_CLIENTS $MAX_CLIENTS
 ./run-client.sh $SERVER_HOST "16" "125" "1" true "16" $MIN_CLIENTS $MAX_CLIENTS
 ./run-client.sh $SERVER_HOST "16" "125" "2" false "0" $MIN_CLIENTS $MAX_CLIENTS
 ./run-client.sh $SERVER_HOST "16" "125" "2" true "0" $MIN_CLIENTS $MAX_CLIENTS
 ./run-client.sh $SERVER_HOST "16" "125" "2" true "1" $MIN_CLIENTS $MAX_CLIENTS
+./run-client.sh $SERVER_HOST "16" "125" "2" true "4" $MIN_CLIENTS $MAX_CLIENTS
 ./run-client.sh $SERVER_HOST "16" "125" "2" true "8" $MIN_CLIENTS $MAX_CLIENTS
 ./run-client.sh $SERVER_HOST "16" "125" "2" true "16" $MIN_CLIENTS $MAX_CLIENTS
 echo "Done"
--- experiment-threads-server.sh
+++ experiment-threads-server.sh
@@ -6,11 +6,13 @@ MAX_CLIENTS=32
 ./run-server.sh $SERVER_HOST false "0" "16" "1" "125" $MIN_CLIENTS $MAX_CLIENTS
 ./run-server.sh $SERVER_HOST true "0" "16" "1" "125" $MIN_CLIENTS $MAX_CLIENTS
 ./run-server.sh $SERVER_HOST true "1" "16" "1" "125" $MIN_CLIENTS $MAX_CLIENTS
+./run-server.sh $SERVER_HOST true "4" "16" "1" "125" $MIN_CLIENTS $MAX_CLIENTS
 ./run-server.sh $SERVER_HOST true "8" "16" "1" "125" $MIN_CLIENTS $MAX_CLIENTS
 ./run-server.sh $SERVER_HOST true "16" "16" "1" "125" $MIN_CLIENTS $MAX_CLIENTS
 ./run-server.sh $SERVER_HOST false "0" "16" "2" "125" $MIN_CLIENTS $MAX_CLIENTS
 ./run-server.sh $SERVER_HOST true "0" "16" "2" "125" $MIN_CLIENTS $MAX_CLIENTS
 ./run-server.sh $SERVER_HOST true "1" "16" "2" "125" $MIN_CLIENTS $MAX_CLIENTS
+./run-server.sh $SERVER_HOST true "4" "16" "2" "125" $MIN_CLIENTS $MAX_CLIENTS
 ./run-server.sh $SERVER_HOST true "8" "16" "2" "125" $MIN_CLIENTS $MAX_CLIENTS
 ./run-server.sh $SERVER_HOST true "16" "16" "2" "125" $MIN_CLIENTS $MAX_CLIENTS
 echo "Done"
