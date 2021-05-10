8a2ae27dec4f14ee734e20739da3282bfb168c08
join <join@itu.dk>
2021-05-10 13:15:16 +0200

    increased experiment size

--- experiment-documents-server.sh
+++ experiment-documents-server.sh
@@ -3,7 +3,7 @@ SERVER_HOST=$1
 index_ops=$2
 THREADS="4"
 NUMBER_OF_CLIENTS=16
-declare -a OPS=(300 90 55 36 25 21 19 16)
+declare -a OPS=(1000 120 60 40 30 25 20 17)
 dsize=$((4+index_ops*36))
 
 for (( file=1; file<=2; file++ ))
