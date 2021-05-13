17e11771eb8dfb32c8b9936fa85874fc74f528bc
join <join@itu.dk>
2021-05-13 07:29:21 +0200

    see last

--- experiment-documents-server.sh
+++ experiment-documents-server.sh
@@ -3,7 +3,7 @@ SERVER_HOST=$1
 index_ops=$2
 THREADS="8"
 NUMBER_OF_CLIENTS=24
-declare -a OPS=(400 80 40 35 22 20 18 15)
+declare -a OPS=(600 100 50 40 25 22 20 16)
 dsize=$((4+index_ops*36))
 
 for (( file=1; file<=2; file++ ))
